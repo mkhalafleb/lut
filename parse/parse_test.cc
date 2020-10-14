1e96c63584e909582053a613e7770bc84a2ee724
Mario Khalaf <mkhalaf@google.com>
2020-10-14 21:49:14 +0000

    Converted to Bazel

---
+++ parse/parse_test.cc
@@ -0,0 +1,25 @@
+#include "parse.h"
+#include "gtest/gtest.h"
+#include <string>
+#include <optional>
+
+namespace {
+
+// The fixture for testing class TextReader 
+class ParseTest : public ::testing::Test {
+ protected:
+};
+
+// Tests that the Foo::Bar() method does Abc.
+TEST_F(ParseTest, LEGALPARSE) {
+  std::string hex_legal("ffee");
+
+  std::optional<uint16_t> val = lut_arg_parser::parse_hex(hex_legal);
+  ASSERT_TRUE(val.has_value());
+  ASSERT_EQ(*val, 0xffee);
+}
+
+
+
+}  // namespace
+
