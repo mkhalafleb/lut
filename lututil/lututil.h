a1e3a755af61450d1ff30337fbc0f547d1019267
Mario Khalaf <mkhalaf@google.com>
2021-01-19 13:49:04 -0500

    More Cleanup

--- lututil/lututil.h
+++ lututil/lututil.h
@@ -3,7 +3,7 @@
 #include "lutmask/lutmask.h"
 
 #ifndef LUT_LUTUTIL_H
-#define LUT_LITUTIL_H
+#define LUT_LUTUTIL_H
 namespace lututil {
 
 std::string print_mask(const lutmask::LutMask &mask, unsigned int pos);
@@ -11,8 +11,6 @@ std::string print_mask(const lutmask::LutMask &mask, unsigned int pos);
 unsigned int calculate_best_pivot(const lutmask::LutMask &mask);
 
 unsigned int find_overall_lit(const lutmask::LutMask &mask, unsigned int pos);
-
-void calculate_new_domain(char *domain_char, int size, int pos);
 
 std::string generate_sop(const lutmask::LutMask &mask);
 
